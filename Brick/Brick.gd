750979e4b7a18ff0ce46d5b4c026bf56195d076c
lily-sivertson <lsiverts@iu.edu>
2023-09-28 18:36:47 -0400

    Complete exercise 3.1

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -6,9 +6,29 @@ var dying = false
 
 func _ready():
 	position = new_position
+	if score>=100:
+		$ColorRect.color=Color8(224,48,49,255)
+	elif score>=90:
+		$ColorRect.color=Color8(255,146,43,255)
+	elif score>=80:
+		$ColorRect.color=Color8(255,212,59,255)
+	elif score>=70:
+		$ColorRect.color=Color8(148,216,45,255)
+	elif score>=60:
+		$ColorRect.color=Color8(34,139,230,255)
+	elif score>=50:
+		$ColorRect.color=Color8(132,94,247,255)
+	elif score>=40:
+		$ColorRect.color=Color8(190,75,219,255)
+	else:
+		$ColorRect.color=Color8(134,142,150,255)
+	
+		
+	
+		
 
 func _physics_process(_delta):
-	if dying:
+	if dying and not $Confetti.emitting:
 		queue_free()
 
 func hit():
@@ -18,5 +38,7 @@ func die():
 	dying = true
 	collision_layer = 0
 	$ColorRect.hide()
+	$Confetti.emitting=true
 	Global.update_score(score)
 	get_parent().check_level()
+	
